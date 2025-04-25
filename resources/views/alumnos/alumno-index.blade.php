8315541339408e2c34a4c159809f6e07864e63a0
Ivannini <ivan.nudelstejer1301@alumnos.udg.mx>
2025-04-24 21:45:51 -0600

    cambios a la pagina

---
+++ resources/views/alumnos/alumno-index.blade.php
@@ -0,0 +1,32 @@
+{{-- <x-layouts.app :title="__('Lista Alumnos')"> --}}
+<x-mi-layout titulo="Lista Alumnos">
+    <div class="flex h-full w-full flex-1 flex-col gap-4 rounded-xl">
+        <div class="relative h-full flex-1 overflow-hidden rounded-xl border border-neutral-200 dark:border-neutral-700">
+
+            <div class="alert alert-primary" role="alert">
+                A simple primary alert—check it out!
+            </div>
+
+            <table border="1">
+                <tr>
+                    <th>Nombre</th>
+                    <th>Correo</th>
+                    <th>Secciones</th>
+                </tr>
+
+                @foreach ($alumnos as $alumno)
+                    <tr>
+                        <td><a href="{{ route('alumno.show', $alumno) }}">{{ $alumno->nombre }}</a></td>
+                        <td>{{ $alumno->correo }}</td>
+                        <td>
+                            @foreach ($alumno->secciones as $seccion)
+                                {{ $seccion->nombre }} - {{ $seccion->seccion }}<br>
+                            @endforeach
+                        </td>
+                    </tr>
+                @endforeach
+            </table>
+
+        </div>
+    </div>
+    </x-layouts.app>
